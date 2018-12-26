e237be20bee9f98829edba50bfef10230caa8a8d
AndreasCanAtakan <andreas.atakan@student.uib.no>
2018-12-26 15:37:09 +0100

    Fixed missing members

--- medlemmer/src/js/main.js
+++ medlemmer/src/js/main.js
@@ -6,9 +6,6 @@ import {json} from "./helper/json.js";
 console.info("Running in test mode");
 
 let maingfx = new gfx($(`div#container`), 0, 5, 64, 13);
-window.gfx = maingfx;
-
-
 
 
 
@@ -17,52 +14,24 @@ window.gfx = maingfx;
 // Generating layer manually for testing purpose
 let url = "http://data.urbalurba.com/api/3/action/organization_list?all_fields=true&include_extras=true",
     title = "Medlemmer";
-
 let data = json,
     hash = maingfx.addLayer(title);
 
 for(let urld of data.result) {
-  let _url = `https://cors.io/?https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?outSr=4326&forStorage=false&outFields=*&maxLocations=20&address=${encodeURI(urld.main_adddress)}&f=json`;
-  $.getJSON(_url, (_data, status) => {
-    if(_data.candidates && _data.candidates[0]) {
-      let res = _data.candidates[0].location;
-      let markerId = maingfx.addMarker(res.y, res.x,
-                                      `<h5><a href=\"${urld.website}\" target=\"_blank\">${urld.display_name}</a></h5>
-                                        <p><b>type</b>: ${urld.organization_type}</p>
-                                        <p><b>beskrivelse</b>: ${urld.description}</p>
-                                        <p>
-                                          <b>Kontakt:</b> ${urld.contact_name} , ${urld.contact_title} <br>
-                                                     <a href=\"tel:${urld.contact_mobile}\" target=\"_blank\">${urld.contact_mobile}</a> <br>
-                                                     <a href=\"mailto:${urld.contact_email}\" target=\"_blank\">${urld.contact_email}</a>
-                                          </p>`,
-                                        hash);
-    }else
-      console.log(urld.main_adddress);
-  });
+  if(urld.latlng.lat != 0 && urld.latlng.lng != 0) {
+    let markerId = maingfx.addMarker(urld.latlng.lat, urld.latlng.lng,
+                                    `<h5><a href=\"${urld.website}\" target=\"_blank\">${urld.display_name}</a></h5>
+                                      <img src=\"${urld.image_url}\" alt=\"\" id=\"organization_logo\">
+                                      <p><b>type</b>: ${urld.organization_type}</p>
+                                      <p><b>beskrivelse</b>: ${urld.description}</p>
+                                      <p>
+                                        <b>Kontakt:</b> ${urld.contact_name} , ${urld.contact_title} <br>
+                                                        <a href=\"tel:${urld.contact_mobile}\" target=\"_blank\">${urld.contact_mobile}</a> <br>
+                                                        <a href=\"mailto:${urld.contact_email}\" target=\"_blank\">${urld.contact_email}</a>
+                                      </p>`,
+                                    hash);
+  }
 }
-
-/*
-for(let urld of data.result) {
-  setTimeout(() => {
-    L.esri.Geocoding.geocode().address(urld.main_adddress).run((err, results, response) => {
-      if(results && results.results) {
-        let res = results.results[0];
-        let markerId = maingfx.addMarker(res.latlng.lat, res.latlng.lng,
-                                        `<h5><a href=\"${urld.website}\" target=\"_blank\">${urld.display_name}</a></h5>
-                                          <p><b>type</b>: ${urld.organization_type}</p>
-                                          <p><b>beskrivelse</b>: ${urld.description}</p>
-                                          <p>
-                                            <b>Kontakt:</b> ${urld.contact_name} , ${urld.contact_title} <br>
-                                                       <a href=\"tel:${urld.contact_mobile}\" target=\"_blank\">${urld.contact_mobile}</a> <br>
-                                                       <a href=\"mailto:${urld.contact_email}\" target=\"_blank\">${urld.contact_email}</a>
-                                            </p>`,
-                                          hash);
-      }else
-        console.log(urld.main_adddress);
-    });
-  }, 3000);
-}
-*/
 
 /*
 $.getJSON(url, (data, status) => {
