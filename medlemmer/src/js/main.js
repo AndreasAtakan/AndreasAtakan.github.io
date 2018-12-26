18f74be8ff3c2f832f14df9c3ff0f413a4331c71
AndreasCanAtakan <andreas.atakan@student.uib.no>
2018-12-26 12:43:39 +0100

    More debugging

--- medlemmer/src/js/main.js
+++ medlemmer/src/js/main.js
@@ -22,6 +22,27 @@ let data = json,
     hash = maingfx.addLayer(title);
 
 for(let urld of data.result) {
+  let _url = `https://cors.io/?https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?outSr=4326&forStorage=false&outFields=*&maxLocations=20&address=${encodeURI(urld.main_adddress)}&f=json`;
+  $.getJSON(_url, (_data, status) => {
+    if(_data.candidates && _data.candidates[0]) {
+      let res = _data.candidates[0].location;
+      let markerId = maingfx.addMarker(res.y, res.x,
+                                      `<h5><a href=\"${urld.website}\" target=\"_blank\">${urld.display_name}</a></h5>
+                                        <p><b>type</b>: ${urld.organization_type}</p>
+                                        <p><b>beskrivelse</b>: ${urld.description}</p>
+                                        <p>
+                                          <b>Kontakt:</b> ${urld.contact_name} , ${urld.contact_title} <br>
+                                                     <a href=\"tel:${urld.contact_mobile}\" target=\"_blank\">${urld.contact_mobile}</a> <br>
+                                                     <a href=\"mailto:${urld.contact_email}\" target=\"_blank\">${urld.contact_email}</a>
+                                          </p>`,
+                                        hash);
+    }else
+      console.log(urld.main_adddress);
+  });
+}
+
+/*
+for(let urld of data.result) {
   setTimeout(() => {
     L.esri.Geocoding.geocode().address(urld.main_adddress).run((err, results, response) => {
       if(results && results.results) {
@@ -41,6 +62,7 @@ for(let urld of data.result) {
     });
   }, 3000);
 }
+*/
 
 /*
 $.getJSON(url, (data, status) => {
